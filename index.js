f9607bf8f29ec3ef68a037b165b27e8728f1d293
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-07 22:30:33 +0530

    rendering a html page

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const http = require("http");
 
+const HTML = `
+<!DOCTYPE html>
+<html lang="en">  
+  <head>      
+    <meta charset="utf-8">
+    <title>My Hello World</title>
+    <style>
+      body {
+        background-color: black;
+        color: yellow;
+        text-align: center;
+        font-size: 40px;
+      }
+    </style>
+  </head>
+  <body>
+    Hello World
+  </body>
+</html>
+`;
+
 const server = http.createServer(function (request, response) {
   response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end("Hello World");
+  response.setHeader("Content-Type", "text/html");
+  response.end(HTML);
 });
 
 server.listen({ port: 3000, host: "localhost" }, function () {
